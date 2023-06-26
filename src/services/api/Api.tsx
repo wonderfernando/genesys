4fc1ae297e6c885b0bfc99cd26ba228ccf419070
fernandowonder <fernando.wonder12345@gmail.com>
2023-06-26 17:20:15 +0100

    alteracoes no base api

--- src/services/api/Api.tsx
+++ src/services/api/Api.tsx
@@ -1,1 +1,2 @@
-export const URL_BASE = "http://127.0.0.1:8000/api";
+//export const URL_BASE = "http://127.0.0.1:8000/api";
+export const URL_BASE = "https://superordinary-noses.000webhostapp.com/api";
